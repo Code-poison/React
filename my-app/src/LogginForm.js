7e14f9caf4ca50446f7ca7700659f594af782110
jiajun.chou <jiajun@104.com.tw>
2018-01-22 01:03:40 +0800

    Loggin Form test

---
+++ my-app/src/LogginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Acount extends Component {
+  render (){
+    return (
+      <div className="Acount-Input">
+        <label>Account: </label><input id="account" value="" type="text"></input>
+      </div>
+    );
+  }
+}
+
+export default Acount;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Acount from './LogginForm';
+import Clock from './tick';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Acount/>, document.getElementById('Acount-Input'));
+ReactDOM.render(<Acount/>, document.getElementById('Acount-Input2'));
+ReactDOM.render(<Clock />, document.getElementById('ClickRoot'));
 registerServiceWorker();
