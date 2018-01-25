1ad216acfd77c4befcde473bfc326b5030dd3f2a
jiajun.chou <jiajun@104.com.tw>
2018-01-26 00:57:47 +0800

    Forms: Loggin

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -236,3 +236,40 @@ ReactDOM.render(
     <Toggle />,
     document.getElementById("Toggle")
 )
+
+/**Loggin Forms */
+
+class Loggin extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event){
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                  Account: 
+                  <input type="text" value={this.state.value} onChange={this.handleChange}/>
+              </label>
+              <input type="submit" value="submit" />
+            </form>
+        )        
+    }
+}
+ReactDOM.render(
+    <Loggin />,
+    document.getElementById('Loggin')
+)
