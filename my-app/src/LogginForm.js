901c2967c592bb92c5b08319a69a2d3a5f674783
jiajun.chou <jiajun@104.com.tw>
2018-01-31 14:46:20 +0800

    新增 form 表單

--- my-app/src/LogginForm.js
+++ my-app/src/LogginForm.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 
-class Acount extends Component {
+class LoggingForm extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      account: ""
+    , passwords: ""
+    };
+  }
+
+  handleChangeA = (event) => {
+    this.setState({account: event.target.value});
+  }
+
+  handleCangeP = (event) => {
+    this.setState({passwords: event.target.value});
+  }
+
+  submit = (event) => {
+    console.log("Account: " + this.state.account + "  " + "PassWords: " + this.state.passwords);
+    event.preventDefault();
+  }
+
   render (){
     return (
-      <div className="Acount-Input">
-        <label>Account: </label><input id="account" value="" type="text"></input>
-      </div>
+      <form>
+        <label>
+          Account: 
+          <input type="text" name="account" value={this.state.value} onChange={this.handleChangeA}/>
+        </label>
+        <br />
+        <label>
+          Passwords: 
+          <input type="text" name="passwords" value={this.state.value} onChange={this.handleCangeP}/>
+        </label>
+        <br />
+        <input type="submit" name="submit" value="確認" onClick={this.submit}/>
+      </form>
     );
   }
 }
 
-export default Acount;
+export default LoggingForm;
